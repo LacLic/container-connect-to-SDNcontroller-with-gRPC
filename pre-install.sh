d367e7c742a1deb8060a3bd2267badcd0deb79e4
LacLic <281885421@qq.com>
2021-07-01 18:02:48 +0800

    add pre-install.sh

---
+++ pre-install.sh
@@ -0,0 +1,11 @@
+apt update
+apt install python-pip
+pip install grpcio -i https://mirrors.aliyun.com/pypi/simple/ 
+pip install grpcio-tools -i https://mirrors.aliyun.com/pypi/simple/ 
+apt install mysql-client-core-5.7
+apt install mysql-client-5.7
+apt install mysql-server-core-5.7
+apt install mysql-server-5.7
+apt install libmysqlclient-dev
+pip install MySQL-python -i https://mirrors.aliyun.com/pypi/simple/
+pip install sqlalchemy -i https://mirrors.aliyun.com/pypi/simple/
